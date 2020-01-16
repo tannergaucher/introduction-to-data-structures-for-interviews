4d2529bb0ee691b30aa46122ec4938b16f848104
Tanner Gaucher <tannermichaelgaucher@gmail.com>
2020-01-16 18:07:25 -0500

    Do linked list

--- 03_linked-list-implementation.js
+++ 03_linked-list-implementation.js
@@ -1,35 +1,50 @@
 class LinkedList {
-  constructor() {
-    this._storage = {};
-    this._length = 0;
+  constructor(value) {
+    this.head = { value, next: null }
+    this.tail = this.head
   }
 
-  //   Inserts a new value at the end of the linked list. Constant  time.
-  insert() {
-    // insert value at [storage.length]: value
-    // how to link it to previous value
-    this._storage[this.length] = value;
+  // Inserts a new value to the end of the linked list.
+  insert(value) {
+    const node = { value, next: null }
+    // Set the next value of the current tail to our new node
+    this.tail.next = node
+    // Update the tail to be the real tail, which is the new node.
+    this.tail = node
   }
 
-  //   Deletes a node. Constant time.
-  remove(value) {
-    delete this._storage[value];
-  }
+  // Deletes a node, given a reference to a node.
+  remove() {}
 
-  // Is  a  linear time operation, bc need to loop through all the nodes.
-  contains(searchValue) {
-    const found = false;
-
-    for (let value in this._storage) {
-      if (this._storage[value] === searchValue) {
-        found = true;
-      }
+  // Removes the value at the end of the linked list.
+  removeTail() {
+    // Loop through and get to the node right before the tail.
+    // THIS IS HOW TO TRAVERSE A LINKED LIST.
+    let currentNode = this.head
+    while (currentNode.next !== this.tail) {
+      currentNode = currentNode.next
     }
 
-    return found;
+    currentNode.next = null
+    this.tail = currentNode
+  }
+
+  isHead(node) {
+    return node === this.head
+  }
+
+  isTail(node) {
+    return node === this.tail
+  }
+
+  // Searches linked list and returns true of it contains the value passed.
+
+  contains(value) {
+    let currentNode = this.head
+    while (currentNode.value !== value) {
+      currentNode = currentNode.next
+    }
+
+    return currentNode.value === value
   }
 }
-
-const myLinkedList = new LinkedList();
-
-console.log(myLinkedList);
